1eceaedb5d9fb5b98120d0f5e38d4dc766d546e6
Vincent506 <vicenteneto0806@gmail.com>
2025-07-26 18:52:23 -0300

    refactor

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.cinema {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.cinema to javafx.fxml;
-    exports com.example.cinema;
-}
